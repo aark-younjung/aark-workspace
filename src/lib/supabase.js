713e0976ebbfd95143c09da9b75f10167bf6c5d6
Aark Dev <dev@aark.com.tw>
2026-03-19 21:33:07 +0800

    Fix: Update Supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mfnatapmmmgytrjvwcxy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbmF0YXBtbW1neXRyand3Y3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NTMyMDAsImV4cCI6MjA1NjQyOTIwMH0.K4AXxMHVd0VqeFR9gUGMSyq-zVYHj4pH1vsH5KGT1GE'
+const supabaseUrl = 'https://mekmvytdzxmwjvpnmike.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFla3std29ya3NwYWNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2MTc2ODAsImV4cCI6MjA1NjE5MzY4MH0.K4AXxMHVd0VqeFR9gUGMSyq-zVYHj4pH1vM0aZ8n5Q'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
